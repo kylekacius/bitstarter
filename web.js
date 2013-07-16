718b868c43cb114f7b8b1ac7780a1ee3b22c9696
Kyle Kacius <kylepkacius@gmail.com>
2013-07-16 00:33:31 +0000

    TEST

--- web.js
+++ web.js
@@ -5,12 +5,11 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.send('Hello Wasdfadforld 2!');
 
-fs.readFileSync(index.html);
+var fileinput = fs.readFileSync(index.html);
 
 
-
-
-buffer.toString("utf-8", 0, 12)
+var test = new Buffer(fileinput);
+test.toString("utf-8", 0, 12)
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
