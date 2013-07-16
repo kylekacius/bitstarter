2d4f844de5ab82b5a21efbe07e7f5123185a3754
Kyle Kacius <kylepkacius@gmail.com>
2013-07-16 00:01:30 +0000

    Changed

--- web.js
+++ web.js
@@ -3,9 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello Wasdfadforld 2!');
+
+fs.writeFile('index.html', 'Hello Node', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
 });
 
+buffer.toString("utf-8", 0, 12)
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
